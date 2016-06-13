df782833cf05b4f2e15aabd338670a8b226ecec1
Tyler Forbes <Tyler Forbes>
2016-06-13 16:05:14 -0700

    changed theme name

--- themes/shanti/footer.php
+++ themes/shanti/footer.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying the footer.
  *
- * @package RED_Starter_Theme
+ * @package Shanti_Uganda_Theme
  */
 
 ?>
--- themes/shanti/index.php
+++ themes/shanti/index.php
@@ -2,7 +2,7 @@
 /**
  * The main template file.
  *
- * @package RED_Starter_Theme
+ * @package Shanti_Uganda_Theme
  */
 
 get_header(); ?>
