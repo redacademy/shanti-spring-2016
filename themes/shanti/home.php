e41572c915014379aa34ac6d48b7fef63afb2355
hollycarter <hollycarter342@gmail.com>
2016-06-14 14:36:08 -0700

    Layout and styling of the blog archive.

--- themes/shanti/home.php
+++ themes/shanti/home.php
@@ -2,7 +2,7 @@
 /**
  * The main template file.
  *
- * @package RED_Starter_Theme
+ * @package Shanti_Uganda_Theme
  */
 
 get_header(); ?>
@@ -12,82 +12,50 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-<<<<<<< HEAD
+
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-        <?php
-        /**
-         * Template part for displaying posts.
-         *
-         * @package RED_Starter_Theme
-         */
 
-        ?>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="blog-wrap">
+						<header class="entry-header">
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        	<header class="entry-header">
-        		<?php if ( has_post_thumbnail() ) : ?>
-        			<?php the_post_thumbnail( 'large' ); ?>
-        		<?php endif; ?>
+							<?php if ( has_post_thumbnail() ) : ?>
+								<div class="circle-image">
+									<?php the_post_thumbnail( 'large' ); ?>
+								</div>
+							<?php endif; ?>
 
-        		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+						</header><!-- .entry-header -->
 
-        		<?php if ( 'post' === get_post_type() ) : ?>
-        		<div class="entry-meta">
-        			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-        		</div><!-- .entry-meta -->
-        		<?php endif; ?>
-        	</header><!-- .entry-header -->
+					<div class="entry-content">
+						 <div class="blog-post-title"><?php the_title( ); ?>
+						 </div>
 
-        	<div class="entry-content">
-        		<?php the_content(); ?>
-        	</div><!-- .entry-content -->
-        </article><!-- #post-## -->
+            <div class="blog-date-title">
+							<?php red_starter_posted_on(); ?>
+						</div>
+						<?php the_content(); ?>
+					</div><!-- .entry-content -->
+				</div><!-- .blog-wrap -->
+      </article><!-- #post-## -->
 
 			<?php endwhile; ?>
 
 			<?php the_posts_navigation(); ?>
-=======
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-
-						<section class="infographic">
-							<h2>Our Accomplishments</h2>
-							<ul>
-								<?php $infographic = CFS()->get('infographic'); //create an array of the infographic images
-										foreach($infographic as $pic):?>
-
-										<li><img src="<?php echo $pic['image']; ?>" alt="<?php echo esc_attr($pic['text']); ?>" /></li>
-								 <?php endforeach;?>
-						 </ul>
-						</section>
-
-					</div><!-- .entry-content -->
-				</article><!-- #post-## -->
-
-			<?php endwhile; ?>
-
->>>>>>> be193945b0a68e790b2aecf84924693f6bd15a56
 
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
-
+<div class="newsletter-tag">
+	<p>Stay updated and sign up for our newsletter and blog</p>
+</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-<<<<<<< HEAD
 
-=======
->>>>>>> be193945b0a68e790b2aecf84924693f6bd15a56
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
