80ffc0cb7bd7a8eeb971b51d535e3f0117e78bb6
Tyler Forbes <Tyler Forbes>
2016-06-14 10:48:39 -0700

    completed overlay function of footer

--- themes/shanti/footer.php
+++ themes/shanti/footer.php
@@ -9,6 +9,22 @@
 
 		</div><!-- #content -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="footer_recent-posts">
+				<div class="footer_recent-posts_content">
+				recent post loop goes here
+				</div>
+			</div>
+			<div class="footer_subscribe">
+				<div class="footer_subscribe_content">
+					<h2>
+						Sign up to hear from us!
+					</h2>
+					<form>
+	     			<input type="text" required></input>
+	     			<button type="submit">Subscribe</button>
+					</form>
+				</div>
+			</div>
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
