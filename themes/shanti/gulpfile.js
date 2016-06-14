fd11b0d8ef64d5baf78fefa63525f6071a40297c
hollycarter <hollycarter342@gmail.com>
2016-06-14 14:38:02 -0700

    update gitignore

--- themes/shanti/gulpfile.js
+++ themes/shanti/gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('browser-sync', function() {
    ];
 
     browserSync.init(files, {
-        proxy: 'shanti.dev',
+        proxy: 'http://localhost:8888/shanti-uganda',
     });
 
     gulp.watch(files).on('change', browserSync.reload);
