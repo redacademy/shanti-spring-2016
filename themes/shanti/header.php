00dc6dd94ccfacc73c6d780ae9448c3938a89ff7
Sara <given.sara@gmail.com>
2016-06-14 20:30:08 -0700

    hid nav button for now

--- themes/shanti/header.php
+++ themes/shanti/header.php
@@ -31,7 +31,7 @@
 			<!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">...</button>
+					<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">...</button> -->
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
