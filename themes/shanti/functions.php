c6ece290516f272e25886b99befc78fac7605011
Sara <given.sara@gmail.com>
2016-06-16 10:14:46 -0700

    click function for the mobile navigation menu

--- themes/shanti/functions.php
+++ themes/shanti/functions.php
@@ -89,6 +89,10 @@ function shanti_scripts() {
 
 	wp_enqueue_script( 'shanti-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
+	wp_enqueue_script('jquery');
+
+	wp_enqueue_script('shanti-menu-toggle', get_template_directory_uri() . '/js/menu-toggle.js', array('jquery'), false, true);
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
