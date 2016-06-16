35bbcd652e2944cb97c961f017b911f4212769af
Sara <given.sara@gmail.com>
2016-06-16 15:29:32 -0700

    fix bug in js for menu toggle

--- themes/shanti/js/menu-toggle.js
+++ themes/shanti/js/menu-toggle.js
@@ -3,17 +3,23 @@ jQuery(document).ready(function($) {
   $menu_button = $('button.menu-toggle');
   $main_nav = $('nav.main-navigation');
   $main_nav_items = $('.main-navigation>div>ul>li>a');
-
+  $menu_open = false;
 
   $menu_button.on('click', function(event){
     event.preventDefault();
     $main_nav.toggleClass('toggled');
+    $menu_open = !$menu_open;
   });
+
+
 
   $main_nav_items.on('click', function(event) {
-    event.preventDefault();
-    console.log($(this).parent().children('ul'));
-    $(this).parent().children('ul').toggle();
-  });
 
+    if ($menu_open) {
+      event.preventDefault();
+      console.log($(this).parent().children('ul'));
+      $(this).parent().children('ul').toggle();
+    }
+
+  });
 });
