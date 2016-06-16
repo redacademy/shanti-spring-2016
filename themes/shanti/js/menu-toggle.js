89e56c6bb17ad74d1e53340d0e502065582e0e12
Sara <given.sara@gmail.com>
2016-06-16 11:48:35 -0700

    mobile menu toggle function to show and hide submenus

--- themes/shanti/js/menu-toggle.js
+++ themes/shanti/js/menu-toggle.js
@@ -2,10 +2,18 @@ jQuery(document).ready(function($) {
 
   $menu_button = $('button.menu-toggle');
   $main_nav = $('nav.main-navigation');
+  $main_nav_items = $('.main-navigation>div>ul>li>a');
+
 
   $menu_button.on('click', function(event){
     event.preventDefault();
     $main_nav.toggleClass('toggled');
+  });
+
+  $main_nav_items.on('click', function(event) {
+    event.preventDefault();
+    console.log($(this).parent().children('ul'));
+    $(this).parent().children('ul').toggle();
   });
 
 });
