3cd8a3747d02f3a9fbd1867c48298d7c57f8511f
Sara <given.sara@gmail.com>
2016-06-17 15:54:33 -0700

    set up taxonomy archive pages and pull the correct content into them

--- themes/shanti/taxonomy-action_price.php
+++ themes/shanti/taxonomy-action_price.php
@@ -1,5 +1,12 @@
 <?php
 /**
+ * The template for displaying archive pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?><?php
+/**
  * The template for displaying custom taxonomy .
  *
  * @package RED_Starter_Theme
@@ -38,6 +45,42 @@ get_header(); ?>
 
 			<?php endwhile; ?>
 
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
+
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					get_template_part( 'template-parts/content' );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
 
 		<?php else : ?>
 
