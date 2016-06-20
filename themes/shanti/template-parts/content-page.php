71180c883852511f75a2400cf198cec81a9288b4
Sara <given.sara@gmail.com>
2016-06-20 09:58:41 -0700

    hero image and sub-header color block for the default page template

--- themes/shanti/template-parts/content-page.php
+++ themes/shanti/template-parts/content-page.php
@@ -8,17 +8,18 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<header class="entry-header hero">
 
-	<div class="entry-content">
+
+
+	</header><!-- .entry-header -->
+	<div class="feature-color sub-header">
+		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+		<?php echo CFS()->get('excerpt'); ?>
+	</div>
+
+	<div class="entry-content container">
 		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
+
 	</div><!-- .entry-content -->
 </article><!-- #post-## -->
