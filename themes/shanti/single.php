e9921fc40291e58a41d38492f7a84c22d5ae4b12
hollycarter <hollycarter342@gmail.com>
2016-06-20 14:47:27 -0700

    Styling the single blog pages

--- themes/shanti/single.php
+++ themes/shanti/single.php
@@ -7,12 +7,56 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area container">
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+								<?php if ( has_post_thumbnail() ) : ?>
+									<div class="circle-image">
+															 <?php the_post_thumbnail( 'large' ); ?>
+									</div>
+								<?php endif; ?>
+
+								 <div class="entry-content container">
+									 <div class="blog-date-title">
+										 <span class="blog-post-title"><?php the_title(); ?></span>
+										 <span><?php shanti_posted_on(); ?></span>
+									 </div>
+								<?php the_content(); ?>
+						</div><!-- .entry-content -->
+					</article><!-- #post-## -->
+<!-- <div class="article-content">
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="circle-image">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'large' ); ?>
+					<?php endif; ?>
+
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+					<div class="entry-meta">
+						<?php shanti_posted_on(); ?> / <?php shanti_comment_count(); ?> / <?php shanti_posted_by(); ?>
+					</div><!-- .entry-meta -->
+				</header><!-- .entry-header -->
+
+				<div class="blog-content">
+					<?php the_content(); ?>
+					<?php
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
+</div>
+				<footer class="entry-footer">
+					<?php shanti_entry_footer(); ?>
+				</footer><!-- .entry-footer -->
+			</article><!-- #post-## --> -->
+
 
 			<?php the_post_navigation(); ?>
 
