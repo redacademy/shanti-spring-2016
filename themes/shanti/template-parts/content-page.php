8b0faf14ae40365b55cc983312e7a22aa1c43ad7
Sara <given.sara@gmail.com>
2016-06-21 14:31:13 -0700

    styling of the color blocks on all pages

--- themes/shanti/template-parts/content-page.php
+++ themes/shanti/template-parts/content-page.php
@@ -9,15 +9,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header hero">
-		
+
 
 	</header><!-- .entry-header -->
 	<div class="feature-color sub-header">
-		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+
 		<?php echo CFS()->get('excerpt'); ?>
 	</div>
 
 	<div class="entry-content container">
+		<?php the_title( '<h2 class="page-title">', '</h2>' ); ?>
 		<?php the_content(); ?>
 
 	</div><!-- .entry-content -->
