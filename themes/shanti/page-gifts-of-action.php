3413708b9f226b1d2a9f190849ac6d4909e38c0d
Sara <given.sara@gmail.com>
2016-06-21 20:21:40 -0700

    increase spacing and padding on gifts of action related pages

--- themes/shanti/page-gifts-of-action.php
+++ themes/shanti/page-gifts-of-action.php
@@ -24,30 +24,31 @@ get_header(); ?>
 
       <!-- Loop through the Custom Taxonomy (Action Types) -->
 
+			<div class="gifts-of-action">
 
-      <?php $gift_types = get_terms('action_types'); ?>
-      <?php foreach( $gift_types as $gift): ?>
-        <section class="alternating">
-          <div class="alt-image">
-            <?php $image_id = get_term_meta($gift->term_id, 'image', true);
-                  $image_data = wp_get_attachment_image_src( $image_id, 'full');
-                  $image = $image_data[0];
-             ?>
-             <img src="<?php echo $image; ?>" alt="<?php echo $gift->name; ?>" />
+	      <?php $gift_types = get_terms('action_types'); ?>
+	      <?php foreach( $gift_types as $gift): ?>
+	        <section class="alternating">
+	          <div class="alt-image">
+	            <?php $image_id = get_term_meta($gift->term_id, 'image', true);
+	                  $image_data = wp_get_attachment_image_src( $image_id, 'full');
+	                  $image = $image_data[0];
+	             ?>
+	             <img src="<?php echo $image; ?>" alt="<?php echo $gift->name; ?>" />
 
-          </div>
-          <div class="alt-info">
-            <a href="<?php echo get_term_link($gift, 'action_types') ?>"><h3>Gifts of <?php echo $gift->name; ?></h3></a>
-            <div class="alt-text">
-              <?php echo $gift->description; ?>
-            </div>
-            <div class="alt-link">
-              <a class="cta buy" href="<?php echo get_term_link($gift, 'action_types') ?>">Buy Now</a>
-            </div>
-          </div>
-        </section>
-      <?php endforeach; ?>
-
+	          </div>
+	          <div class="alt-info">
+	            <a href="<?php echo get_term_link($gift, 'action_types') ?>"><h3>Gifts of <?php echo $gift->name; ?></h3></a>
+	            <div class="alt-text">
+	              <?php echo $gift->description; ?>
+	            </div>
+	            <div class="alt-link">
+	              <a class="cta buy" href="<?php echo get_term_link($gift, 'action_types') ?>">Buy Now</a>
+	            </div>
+	          </div>
+	        </section>
+	      <?php endforeach; ?>
+			</div>
 
 
 		</main><!-- #main -->
