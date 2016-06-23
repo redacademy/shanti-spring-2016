5c4cc0253f315c8dcc00cb7c5b742a29765f3fc2
Sara <given.sara@gmail.com>
2016-06-23 13:09:52 -0700

    fix missing closing tags in footer

--- themes/shanti/footer.php
+++ themes/shanti/footer.php
@@ -10,42 +10,41 @@
 		</div><!-- #content -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
+			<div class="recentPosts">
 
-				<div class="recentPosts">
+				<div class="recentPosts-content">
 
-					<div class="recentPosts-content">
+					<h3><a href="blog">Recent Blog Posts</a></h3>
 
-						<h3><a href="blog">Recent Blog Posts</a></h3>
+					<ul class="blogPosts">
+						<?php
+						   $args = array( 'post_type' => 'post',
+														 	'posts_per_page' => 3);
+						   $blog_posts = get_posts( $args ); // returns an array of posts
+			 			?>
 
-						<ul class="blogPosts">
-							<?php
-							   $args = array( 'post_type' => 'post',
-															 	'posts_per_page' => 3);
-							   $blog_posts = get_posts( $args ); // returns an array of posts
-				 			?>
+					 <?php foreach ( $blog_posts as $post ) : setup_postdata( $post ); ?>
+						 <li>
+							 <a href="<?php esc_url(the_permalink()); ?>">
+							 	<div>
+								 	<?php if ( has_post_thumbnail() ) : ?>
+									 	<?php the_post_thumbnail( 'thumbnail' ); ?>
+								 	<?php endif; ?>
+						 		</div>
+								<h4>
+									<?php echo the_title() ?>
+								</h4>
+								<p>
+									<?php echo wp_kses_post( CFS()->get( 'subtitle' ) ); ?>
+								</p>
+							</a>
+							</li>
+		 	 			<?php endforeach; wp_reset_postdata(); ?>
 
-						 <?php foreach ( $blog_posts as $post ) : setup_postdata( $post ); ?>
-							 <li>
-								 <a href="<?php esc_url(the_permalink()); ?>">
-								 	<div>
-									 	<?php if ( has_post_thumbnail() ) : ?>
-										 	<?php the_post_thumbnail( 'thumbnail' ); ?>
-									 	<?php endif; ?>
-							 		</div>
-									<h4>
-										<?php echo the_title() ?>
-									</h4>
-									<p>
-										<?php echo wp_kses_post( CFS()->get( 'subtitle' ) ); ?>
-									</p>
-								</a>
-								</li>
-			 	 			<?php endforeach; wp_reset_postdata(); ?>
+					</ul>
 
-						</ul>
-
-					</div>
 				</div>
+			</div>
 
 			<div class="subscribe">
 				<div class="subscribe-content">
@@ -58,37 +57,72 @@
 						<div>
 							<div class="subscribe-input">
 								<i class="fa fa-envelope" aria-hidden="true"></i>
-	     					<input type="email" placeholder="Your e-mail Address...."required></input>
+	     					<input type="email" placeholder="Your e-mail Address...." required />
 							</div>
 
 	     				<button type="submit">Subscribe</button>
 						</div>
 					</form>
-					<div class="socialMedia-links">
-						<a href="https://twitter.com/shantiuganda"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-						<a href="https://www.facebook.com/shantiuganda"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-						<a href="https://www.flickr.com/photos/shantiugandasociety/sets/"><i class="fa fa-flickr" aria-hidden="true"></i></a>
-						<a href="https://www.pinterest.com/shantiuganda/"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-						<a href="https://www.youtube.com/user/shantiug"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
-				</div>
-				<div class="insite-links">
-					<a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ) ?>">Blog</a>
-						<span> &bull; </span>
-  				<a href="<?php echo get_page_link(577); ?>">Press</a>
-						<span> &bull; </span>
-					<a href="<?php echo get_page_link(42); ?>">Careers</a>
-						<span> &bull; </span>
- 					<a href="<?php echo get_page_link(50); ?>">Events</a>
-						<span class="hiddenBullet"> &bull; </span>
-					<a href="<?php echo get_page_link(575); ?>">Newsletter</a>
-						<span> &bull; </span>
-					<a href="<?php echo get_page_link(62); ?>">Contact Us</a>
-				</div>
-			</div>
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+
+
+					<div class="socialMedia-links">
+						<a href="https://twitter.com/shantiuganda">
+							<i class="fa fa-twitter" aria-hidden="true"></i>
+						</a>
+						<a href="https://www.facebook.com/shantiuganda">
+							<i class="fa fa-facebook" aria-hidden="true"></i>
+						</a>
+						<a href="https://www.flickr.com/photos/shantiugandasociety/sets/">
+							<i class="fa fa-flickr" aria-hidden="true"></i>
+						</a>
+						<a href="https://www.pinterest.com/shantiuganda/">
+							<i class="fa fa-pinterest" aria-hidden="true"></i>
+						</a>
+						<a href="https://www.youtube.com/user/shantiug">
+							<i class="fa fa-youtube-play" aria-hidden="true"></i>
+						</a>
+					</div> <!-- end socialmedia links -->
+
+					<div class="insite-links">
+
+						<a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ) ?>">
+							Blog
+						</a>
+							<span> &bull; </span>
+
+	  				<a href="<?php echo get_page_link(577); ?>">
+							Press
+						</a>
+							<span> &bull; </span>
+
+						<a href="<?php echo get_page_link(42); ?>">
+							Careers
+						</a>
+							<span> &bull; </span>
+
+	 					<a href="<?php echo get_page_link(50); ?>">
+							Events
+						</a>
+							<span class="hiddenBullet"> &bull; </span>
+
+						<a href="<?php echo get_page_link(575); ?>">
+							Newsletter
+						</a>
+							<span> &bull; </span>
+
+						<a href="<?php echo get_page_link(62); ?>">
+							Contact Us
+						</a>
+
+					</div> <!-- end insite-links -->
+
+				</div> <!-- end subscribe content -->
+			</div> <!-- end subscribe -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
 
 	</body>
 </html>
