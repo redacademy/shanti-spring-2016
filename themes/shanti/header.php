9aba3fcef3c4c0a1a71fc5d1f19a9619e1acd38b
Sara <given.sara@gmail.com>
2016-06-28 14:24:40 -0700

    add meta tags to the header

--- themes/shanti/header.php
+++ themes/shanti/header.php
@@ -10,11 +10,13 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="title" content="<?php bloginfo('name') ?>">
+		<meta name="description" content="<?php bloginfo('description') ?>">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 	<?php wp_head(); ?>
-	
+
 	</head>
 
 	<body <?php body_class(); ?>>
